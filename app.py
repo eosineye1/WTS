969b7ee0af9c4c9995054a0b137ab15620899630
eosineye1 <osineye70@gmail.com>
2021-10-24 23:58:27 -0400

    Removed stop words

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 
 @st.cache(suppress_st_warning=True)
 def generate_summary(text):
-    stopWords = set(stopwords.words("english"))
+    stopWords = [ "i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","can","will","just","don't","should","now"]
     words = word_tokenize(text)
 
     # Creating a frequency table to keep the score of each word
